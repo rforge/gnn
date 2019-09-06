cf95773d598fbec7674846bbefffb3b5278e84e2
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-06 17:23:28 +0000

    minor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/GMMN_QMC.R
+++ pkg/tests/GMMN_QMC.R
@@ -3,5 +3,7 @@
 ## Basic tests of QMC based on GMMNs
 
 library(gnn)
-if(require(tensorflow))
+if(require(tensorflow) &&
+   packageVersion("qrng") >= "0.0-7" &&
+   packageVersion("copula") >= "0.999.19")
     demo("GMMN_QMC")
