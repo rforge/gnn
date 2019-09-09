d2bbe03fdf665e5c4e662d2fc985059a692332e1
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-09 18:02:51 +0000

    another fix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/GMMN_QMC.R
+++ pkg/tests/GMMN_QMC.R
@@ -6,8 +6,10 @@ library(gnn)
 
 ## Check (too restrictive as the OS-level TensorFlow installation will not catch
 ## TensorFlow installations done differently; see the stackoverflow link)
-if(length(system("pip list | grep tensorflow", intern = TRUE)) > 0 && # see https://stackoverflow.com/questions/38549253/how-to-find-which-version-of-tensorflow-is-installed-in-my-system
-   require(tensorflow) &&
+checkTF <- system("pip list | grep tensorflow", intern = TRUE) # see https://stackoverflow.com/questions/38549253/how-to-find-which-version-of-tensorflow-is-installed-in-my-system
+TFisInstalled <- length(checkTF) > 0 && grepl("tensorflow", checkTF[[1]])
+if(TFisInstalled && # OS-level TensorFlow
+   require(tensorflow) && # tensorflow package
    require(qrng) && packageVersion("qrng") >= "0.0-7" &&
    require(copula) && packageVersion("copula") >= "0.999.19")
     demo("GMMN_QMC")
