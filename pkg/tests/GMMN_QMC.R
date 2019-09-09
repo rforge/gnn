00455f8d965ef15634d891059dcbaf67f5a96cb6
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-09 17:46:37 +0000

    fix in demo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/GMMN_QMC.R
+++ pkg/tests/GMMN_QMC.R
@@ -3,7 +3,11 @@
 ## Basic tests of QMC based on GMMNs
 
 library(gnn)
-if(require(tensorflow) &&
-   packageVersion("qrng") >= "0.0-7" &&
-   packageVersion("copula") >= "0.999.19")
+
+## Check (too restrictive as the OS-level TensorFlow installation will not catch
+## TensorFlow installations done differently; see the stackoverflow link)
+if(length(system("pip list | grep tensorflow", intern = TRUE)) > 0 && # see https://stackoverflow.com/questions/38549253/how-to-find-which-version-of-tensorflow-is-installed-in-my-system
+   require(tensorflow) &&
+   require(qrng) && packageVersion("qrng") >= "0.0-7" &&
+   require(copula) && packageVersion("copula") >= "0.999.19")
     demo("GMMN_QMC")
