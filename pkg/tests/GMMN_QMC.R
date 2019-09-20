b79300c950bd78ea7d1eab2589470959f078a816
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-09-20 13:54:32 +0000

    better check whether TF is installed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/GMMN_QMC.R
+++ pkg/tests/GMMN_QMC.R
@@ -6,8 +6,11 @@ library(gnn)
 
 ## Check (too restrictive as the OS-level TensorFlow installation will not catch
 ## TensorFlow installations done differently; see the stackoverflow link)
-checkTF <- system("pip list | grep tensorflow", intern = TRUE) # see https://stackoverflow.com/questions/38549253/how-to-find-which-version-of-tensorflow-is-installed-in-my-system
-TFisInstalled <- length(checkTF) > 0 && grepl("tensorflow", checkTF[[1]])
+checkCMD <- tryCatch(checkTF <- system("pip list | grep tensorflow",
+                                       intern = TRUE, ignore.stderr = TRUE),
+                     error = function(e) e) # see https://stackoverflow.com/questions/38549253/how-to-find-which-version-of-tensorflow-is-installed-in-my-system
+TFisInstalled <- !is(checkCMD, "simpleError") && length(checkTF) > 0 &&
+    grepl("tensorflow", checkTF[[1]])
 if(TFisInstalled && # OS-level TensorFlow
    require(tensorflow) && # tensorflow package
    require(qrng) && packageVersion("qrng") >= "0.0-7" &&
