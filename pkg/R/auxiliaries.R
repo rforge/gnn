a5e00828b9cbd373343a7f34c3795d8548e49683
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-03-03 16:10:52 +0000

    for now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@124 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/auxiliaries.R
+++ pkg/R/auxiliaries.R
@@ -35,21 +35,21 @@ rm_ext <- function(x)
 ##' @return timings in human-readable format
 ##' @author Marius Hofert
 human_time <- function(expr, digits = 2, ...) {
-    toHuman <- function(t)
-        if(t < 60) {
-            paste0(round(t, digits = digits),"s")
+    toHuman <- function(tm) {
+        if(!is.numeric(tm))
+            stop("'tm' not numeric but ",class(tm))
+        if(tm < 60) {
+            paste0(round(tm, digits = digits),"s")
         } else if(t < 3600) {
-            paste0(round(t/60, digits = digits),"min")
+            paste0(round(tm/60, digits = digits),"min")
         } else {
-            paste0(round(t/3600, digits = digits),"h")
+            paste0(round(tm/3600, digits = digits),"h")
         }
+    }
     res <- sapply(system.time(expr, ...), toHuman)
     res <- res[1:3]
     names(res) <- c("user", "system", "elapsed")
     noquote(res)
 }
-
-
-
 
 
