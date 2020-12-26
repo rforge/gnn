8f025060514f88b7b64ed45a18d2fc5b271eaf38
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-12-26 23:24:35 +0000

    fixed saved NNs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@236 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/zzz.R
@@ -0,0 +1,16 @@
+## Function to be executed when package is loaded (or attached)
+.onLoad <- function(libname, pkgname) {
+    ## Avoid startup message of layer_dense() in GMMN() if TensorFlow is not
+    ## built with CPU extensions such as AVX (speed improvements on CPUs)
+    ## See https://stackoverflow.com/questions/65453529/suppress-hard-to-catch-output-of-an-r-function
+    ## https://github.com/rstudio/tensorflow/issues/411
+    ## https://stackoverflow.com/questions/35869137/avoid-tensorflow-print-on-standard-error
+    ## https://stackoverflow.com/questions/47068709/your-cpu-supports-instructions-that-this-tensorflow-binary-was-not-compiled-to-u
+    ## Note: 0 = default;
+    ##       1 = avoid messages;
+    ##       2 = avoid messages and warnings;
+    ##       3 = avoid messages, warnings and errors.
+    Sys.setenv(TF_CPP_MIN_LOG_LEVEL = "1")
+    invisible()
+}
+
