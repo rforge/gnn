a8b59183702694f424732a8b35da1aad310f8f34
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-01-04 22:24:14 +0000

    renamings
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@263 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rGNN.R
+++ pkg/R/rGNN.R
@@ -9,23 +9,23 @@ rGNN <- function(x, ...)  UseMethod("rGNN")
 ##' @param x object of S3 class "gnn_GNN" to be sampled from (input layer is
 ##'        d-dimensional)
 ##' @param size sample size
-##' @param prior NULL (in which case N(0,1)^d is pseudo-sampled via rPrior())
+##' @param data NULL (in which case N(0,1)^d is pseudo-sampled via rPrior())
 ##'        or a (size, d)-matrix of prior samples.
 ##' @param pobs logical indicating whether pobs() is applied to the output
 ##'        before returning
-##' @param ... additional arguments passed to rPrior() if prior = NULL
+##' @param ... additional arguments passed to rPrior() if data = NULL
 ##' @return Sample from the GNN 'x' (feedforwarded prior sample)
 ##' @author Marius Hofert
-##' @note rGNN.numeric <- function(n, x, prior, ...) would have been another
-##'       option but then 'n' is required even if prior is a sample, which is
+##' @note rGNN.numeric <- function(n, x, data, ...) would have been another
+##'       option but then 'n' is required even if 'data' is a sample, which is
 ##'       weird. And omitting 'n' then leads to error 'no applicable method for
 ##'       'rGNN' applied to an object of class "name"'. The current version acts
 ##'       more like sample()
-rGNN.gnn_GNN <- function(x, size, prior = NULL, pobs = FALSE, ...)
+rGNN.gnn_GNN <- function(x, size, data = NULL, pobs = FALSE, ...)
 {
     stopifnot(inherits(x, "gnn_GNN"), is.logical(pobs))
-    if(is.null(prior))
-        prior <- rPrior(size, copula = indepCopula(dim(x)[1]), ...) # independent N(0,1)
-    res <- ffGNN(x, data = prior)
+    if(is.null(data))
+        data <- rPrior(size, copula = indepCopula(dim(x)[1]), ...) # independent N(0,1)
+    res <- ffGNN(x, data = data)
     if(pobs) pobs(res) else res
 }
