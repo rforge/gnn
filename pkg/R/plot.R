55aa6e5c98bda9e111e6769677efc9d0068d1785
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-02-13 02:22:56 +0000

    fixes in loss plot
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnn@289 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.R
+++ pkg/R/plot.R
@@ -10,7 +10,9 @@
 ##' @note - Could allow to plot (and return) various loss functions (if
 ##'         'x' is a list), but probably of little value
 ##'       - Could also add training time, but not directly loss related
-plot_loss <- function(x, type = "l", xlab = "Epoch", ylab = "Loss", y2lab = NULL, ...)
+plot_loss <- function(x, type = "l", xlab = "Epoch",
+                      ylab = paste0("Loss (",x[["loss.type"]],")"),
+                      y2lab = NULL, ...)
 {
     if(!is.trained.gnn_GNN(x))
         stop("'x' needs to be a trained object of class \"gnn_GNN\"")
